ea535353a592a04ab339f1f2e9d3b1c4fd6314c7
Anastasiia Andrusenko <107683074+Anastasiia-Andrusenko@users.noreply.github.com>
2024-03-02 13:46:02 +0200

    add navlink

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,17 +1,27 @@
 import ListOfTrips from 'components/ListOfTrips/ListOfTrips';
 import css from './Home.module.css';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
+
+
   return (
     <div className={css.container}>
       <section className={css.main}>
         <h1 className={css.title}>Weather  <span className={css.titleBold}>Forecast</span></h1>
         {/* {isLoggedIn ? <UserMenu/> : <p>To plan a trip log in or register</p>} */}
-        <p className={css.caption}>Please LogIn or Register</p>
+        <p className={css.caption}>
+          Please{' '}
+            <NavLink className={css.link} to="/login">LogIn</NavLink>{' '}
+            or <NavLink className={css.link} to="/register">Register</NavLink></p>
         <ListOfTrips/>
       </section>
       <section className={css.aside}>
-        <p>24 C</p>
+        <div className={css.weather}>
+          <p className={css.day}>Saturday</p>
+          <p className={css.temperature}>24<span className={css.degree}> С</span></p>
+          <p className={css.cityName}>Berlin</p>
+        </div>
       </section>
     </div>
   )
